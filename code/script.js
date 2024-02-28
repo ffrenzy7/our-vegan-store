feb57e34d417ee823f206d8fc812bd9f2c29e260
Renan Alonso <renan.alonso91@gmail.com>
2024-02-28 03:10:33 -0300

    feat: mobile menu burger dropdown

--- code/script.js
+++ code/script.js
@@ -1,8 +1,15 @@
 const menu = document.querySelector('.menu')
 const menuMobile = document.querySelector('.menu_mobile')
+const menuMobileDrop = document.querySelector('.menu_mobile_drop')
+const menuDropdown = document.querySelector('.menu_dropdown')
 
 menu.addEventListener('click', openMenu)
+menuMobileDrop.addEventListener('click', dropDown)
 
 function openMenu() {
   menuMobile.classList.toggle('open')
+}
+
+function dropDown() {
+  menuDropdown.classList.toggle('open')
 }
