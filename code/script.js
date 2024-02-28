f6f8493cd7090bb31ba4b840f3f645b671cccbd0
Renan Alonso <renan.alonso91@gmail.com>
2024-02-28 03:34:39 -0300

    feat: dark overlay when menu burger is open

--- code/script.js
+++ code/script.js
@@ -2,12 +2,15 @@ const menu = document.querySelector('.menu')
 const menuMobile = document.querySelector('.menu_mobile')
 const menuMobileDrop = document.querySelector('.menu_mobile_drop')
 const menuDropdown = document.querySelector('.menu_dropdown')
+const overlay = document.querySelector('.overlay')
 
 menu.addEventListener('click', openMenu)
+overlay.addEventListener('click', openMenu)
 menuMobileDrop.addEventListener('click', dropDown)
 
 function openMenu() {
   menuMobile.classList.toggle('open')
+  overlay.classList.toggle('visible')
 }
 
 function dropDown() {
